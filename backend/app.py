602e419031c980d5ecf4fb9b5d53fc00b77d0b76
banuvamsi <133517740+banuvamsi@users.noreply.github.com>
2025-04-08 19:34:54 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,11 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 import random
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
 def home():
-    return "Welcome to GenFarm AI Backend!"
+    return 'Welcome to GenFarm AI Backend!'
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
@@ -28,6 +26,3 @@ def analyze():
         'disease': disease,
         'suggestion': suggestions[disease]
     })
-
-if __name__ == '__main__':
-    app.run(debug=True)
